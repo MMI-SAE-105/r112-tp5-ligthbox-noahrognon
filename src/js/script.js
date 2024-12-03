bc2f79d29c417c9ea9b37d302f3084aea85e2b69
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2024-12-03 17:03:18 +0100

    js finit + css adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const lightBox = document.querySelector('#lightbox');
+    const lightBoxImage = lightBox.querySelector('img');
+    const thumbnails = document.querySelectorAll('ul li img');
+
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', () => {
+            lightBoxImage.src = thumbnail.dataset.fullImg;
+            lightBox.showModal();
+        });
+    });
+
+    lightBox.addEventListener('click', () => {
+        lightBox.classList.add('sortie');
+        lightBox.addEventListener('animationend', () => {
+            lightBox.classList.remove('sortie');
+            lightBox.close();
+        }, { once: true });
+    });
+});
